c535c550f5bfdc03f3e0a6c7c563ad25a0d70339
ryantgoodman <ryantgoodman@gmail.com>
2025-11-30 01:47:25 -0700

    Updated conditionals exercise 1

--- py100/exercise.py
+++ py100/exercise.py
@@ -1,10 +1,20 @@
 # some code will go here
-
+import random
 # Loop on Command
-while True:
-  print("Should I stop looping?")
-  answer = input().lower().strip()
-  if answer == "yes":
-    break
-  else:
-    print("Answer yes to stop looping")
+# while True:
+#   print("Should I stop looping?")
+#   answer = input().lower().strip()
+#   if answer == "yes":
+#     break
+#   else:
+#     print("Answer yes to stop looping")
+
+# Conditionals 
+
+# Yes? No? Part 1
+
+random_number = random.randint(0 , 1)
+if random_number == 1:
+  print("Yes!")
+else:
+  print("No")
